4dc050002dca2140df27578353e9e61824219f25
treasurechic <babychic140@gmail.com>
2019-03-08 17:16:24 +0100

    more features

--- web_development.js
+++ web_development.js
@@ -7,7 +7,7 @@ $(function() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = "none";  
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
